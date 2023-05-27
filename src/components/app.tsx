d4cdce691476b04ca37e2b3629e52ebf9ea1b3d1
Curtis Knudson <curtis@midgard.marketing>
2023-05-26 21:23:03 -0600

    Complete websocket tutorial

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,30 +1,61 @@
-import * as React from "react"
-import { Stats, StatsItem } from "./stats"
-import { useColorScheme } from "../hooks/use-color-scheme"
+import * as React from "react";
+import clsx from "clsx";
+import { Stats, StatsItem } from "./stats";
+import { useColorScheme } from "../hooks/use-color-scheme";
+import { lightThemeClass, darkThemeClass } from "../styles/themes.css";
+import * as styles from "./app.css";
+import "../styles/global.css";
 
 const App: React.FC = () => {
-  const scheme = useColorScheme()
-  const schemeClass = scheme === `dark` ? `$darkThemeClass` : `$lightThemeClass`
-  const [variant, setVariant] = React.useState<"default" | "invert">(`default`)
+  const scheme = useColorScheme();
+  const schemeClass = scheme === `dark` ? darkThemeClass : lightThemeClass;
+  const [variant, setVariant] = React.useState<"default" | "invert">(`default`);
 
+  console.log(variant);
   return (
-    <div className={schemeClass}>
-      <main>
-        <div>
-          <div>Last 30 Days</div>
-          <button type="button" onClick={() => (variant === `default` ? setVariant(`invert`) : setVariant(`default`))}>
+    <div className={clsx(schemeClass, styles.wrapper)}>
+      <main className={styles.innerWrapper}>
+        <div className={styles.topBar}>
+          <div className={styles.topBarHeading[scheme]}>Last 30 Days</div>
+          <button
+            className={styles.button}
+            type="button"
+            onClick={() =>
+              variant === `default`
+                ? setVariant(`invert`)
+                : setVariant(`default`)
+            }
+          >
             Toggle Variant
           </button>
         </div>
         <Stats variant={variant}>
-          <StatsItem label="Total Subscribers" from={70.946} to={71.897} percentage={12} />
-          <StatsItem label="Avg. Open Rate" from={56.14} to={58.16} percentage={2.02} />
-          <StatsItem label="Avg. Click Rate" from={28.62} to={24.57} percentage={-4.05} />
+          <StatsItem
+            label="Total Subscribers"
+            from={70.946}
+            to={71.897}
+            percentage={12}
+          />
+          <StatsItem
+            label="Avg. Open Rate"
+            from={56.14}
+            to={58.16}
+            percentage={2.02}
+          />
+          <StatsItem
+            label="Avg. Click Rate"
+            from={28.62}
+            to={24.57}
+            percentage={-4.05}
+          />
         </Stats>
-        <div>Design by Tailwind UI. Built with vanilla-extract for educational purposes.</div>
+        <div className={styles.footer[scheme]}>
+          Design by Tailwind UI. Built with vanilla-extract for educational
+          purposes.
+        </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,52 +1,93 @@
-import * as React from "react"
+import * as React from "react";
+import { stats, trend } from "../styles/recipes.css";
+import { sprinkles as s } from "../styles/sprinkles.css";
+import { wrapper, toValue, arrowUp, arrowDown } from "./stats.css";
 
 interface IStatsItemProps {
-  label: string
-  from: number
-  to: number
-  percentage: number
+  label: string;
+  from: number;
+  to: number;
+  percentage: number;
 }
 
 interface IStatsProps {
-  variant: "default" | "invert"
+  variant: "default" | "invert";
 }
 
-const StatsContext = React.createContext<IStatsProps["variant"]>(`default`)
+const StatsContext = React.createContext<IStatsProps["variant"]>(`default`);
 
-export const Stats: React.FC<React.PropsWithChildren<IStatsProps>> = ({ children, variant = `default` }) => (
+export const Stats: React.FC<React.PropsWithChildren<IStatsProps>> = ({
+  children,
+  variant = `default`,
+}) => (
   <StatsContext.Provider value={variant}>
-    <section>{children}</section>
+    <section className={stats({ background: variant, color: variant })}>
+      {children}
+    </section>
   </StatsContext.Provider>
-)
+);
 
 export const Trend: React.FC<{ percentage: number }> = ({ percentage }) => {
-  const variant = React.useContext(StatsContext)
-  const isPositive = Math.sign(percentage) === 1
-  const variantPostfix = isPositive ? `Up` : `Down`
+  const variant = React.useContext(StatsContext);
+  const isPositive = Math.sign(percentage) === 1;
+  const variantPostfix = isPositive ? `Up` : `Down`;
 
   return (
-    <div>
-      <span />
-      <span>{Math.abs(percentage)}%</span>
+    <div
+      className={trend({
+        background: `${variant}${variantPostfix}`,
+        color: `${variant}${variantPostfix}`,
+      })}
+    >
+      <span className={isPositive ? arrowUp : arrowDown} />
+      <span className={s({ marginLeft: `xs` })}>{Math.abs(percentage)}%</span>
     </div>
-  )
-}
+  );
+};
 
-export const StatsItem: React.FC<IStatsItemProps> = ({ label, from, to, percentage }) => {
-  const variant = React.useContext(StatsContext)
+// Rest of imports and components...
+
+export const StatsItem: React.FC<IStatsItemProps> = ({
+  label,
+  from,
+  to,
+  percentage,
+}) => {
+  const variant = React.useContext(StatsContext);
 
   return (
-    <div>
-      <div>{label}</div>
-      <div>
-        <div>
-          <span>{to.toFixed(2)}%</span>
+    <div className={wrapper[variant]}>
+      <div className={s({ fontWeight: 600 })}>{label}</div>
+      <div
+        className={s({
+          display: `flex`,
+          alignItems: `center`,
+          justifyContent: `space-between`,
+          marginTop: `md`,
+        })}
+      >
+        <div className={s({ display: `flex`, alignItems: `baseline` })}>
+          <span
+            className={[
+              s({
+                fontSize: `xl`,
+                fontWeight: 600,
+                marginRight: `sm`,
+                lineHeight: `xl`,
+              }),
+              toValue[variant],
+            ].join(` `)}
+          >
+            {to.toFixed(2)}%
+          </span>
           {` `}
           from{` `}
-          <span>{from.toFixed(2)}%</span>
+          <span className={s({ marginLeft: `sm`, fontWeight: 500 })}>
+            {from.toFixed(2)}%
+          </span>
         </div>
         <Trend percentage={percentage} />
       </div>
     </div>
-  )
-}
+  );
+};
